1eaeb702edbabe95cc8ab6bdeee5bc29898e62b2
DeepeshKalura <deepeshkalurs@gmail.com>
2026-01-30 15:00:00 +0530

    feat: updated the new how it works man!!

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,6 @@ import { Hero } from "@/components/Hero";
 import { HowItWorks } from "@/components/HowItWorks";
 import { PricingSection } from "@/components/PricingSection";
 import { ProblemSection } from "@/components/ProblemSection";
-import { UseCases } from "@/components/UseCases";
 
 export default function App() {
   return (
@@ -16,7 +15,6 @@ export default function App() {
       <FeaturesSection />
       <HowItWorks />
       <PricingSection />
-      <UseCases />
       <FAQSection />
       <CTASection />
       <Footer />
