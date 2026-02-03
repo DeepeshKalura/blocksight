bb74a9e2b3d1c64bd02e744e41fd5866a753eea9
DeepeshKalura <deepeshkalurs@gmail.com>
2026-02-03 11:00:00 +0530

    fix: the build error

--- apps/web/lib/db/index.ts
+++ apps/web/lib/db/index.ts
@@ -2,4 +2,10 @@ import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import * as schema from './schema';
 // Use this object to send drizzle queries to your DB
-export const db = drizzle(sql, { schema });
+export const db = (process.env.POSTGRES_URL || process.env.DATABASE_URL)
+    ? drizzle(sql, { schema })
+    : drizzle({
+        // Mock client that does nothing but satisfies type checker for build time
+        // This prevents build errors when environment variables are missing
+        query: async () => [],
+    } as any, { schema });
